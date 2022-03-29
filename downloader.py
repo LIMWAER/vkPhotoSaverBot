06a741c1e1d40a6be76a801f0923b6ee6222abad
LIMWAER <limwaer98@gmail.com>
2022-03-30 02:54:58 +0300

    got some changes, create requirements.txt, move vk api functions into vk_func.py

--- downloader.py
+++ downloader.py
@@ -1,11 +1,27 @@
-import urllib
+from urllib import request
+import os
+import asyncio
 
 
-#TODO: func for downloading photos
-def downloader(*args, path):
-    pass
-    # resource = urllib.urlopen(img)
-    # out = open("...\img.jpg", 'wb')
-    # out.write(resource.read())
-    # out.close()
+def fire_and_forget(f):
+    def wrapped(*args, **kwargs):
+        return asyncio.get_event_loop().run_in_executor(None, f, *args, *kwargs)
 
+    return wrapped
+
+
+@fire_and_forget
+def downloader(album_list_id, path=f'{os.environ["HOMEPATH"]}\\Downloads'):
+    os.mkdir(f'{path}\\Albums')
+    for album in album_list_id:
+        album_path = f'{path}\\Albums\\{album["id"]}'
+        os.mkdir(album_path)
+        for photo in album['photos']:
+            for key in photo:
+                url = photo[key][0]['url']
+                split_name = url[::-1].split("/")
+                photo_name = split_name[0][::-1].split("?")[0]
+                resource = request.urlopen(url)
+                out = open(f'{album_path}\\{photo_name}', 'wb')
+                out.write(resource.read())
+                out.close()
